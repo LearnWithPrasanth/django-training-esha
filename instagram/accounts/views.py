8d5af1849b4f07a7acacc9142106aae6bbc6a580
Prasanth Kumar <training@prasanthkumarnagothi.com>
2025-02-07 10:47:27 +0530

    Update signup view with user registration

--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 
 
 def signup(request):
+    # request_data = request.scheme
+    # print(request_data)
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        User.objects.create(
+            first_name=first_name,
+            username=username,
+            password=password
+        )
     return HttpResponse('<h1>Signup</h1>')
 
 
