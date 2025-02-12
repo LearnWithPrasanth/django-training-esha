0c3da42817c9491a027ecfff05cefcb2949b5889
Prasanth Kumar <training@prasanthkumarnagothi.com>
2025-02-12 10:27:43 +0530

    Models & CRUD Operations

--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -11,6 +11,10 @@ def signup(request):
         first_name = request.POST.get('firstname')
         username = request.POST.get('username')
         password = request.POST.get('password')
+
+        if User.objects.filter(username=username):
+            return render(request, 'accounts/signup.html')
+
         User.objects.create_user(
             first_name=first_name,
             username=username,
@@ -24,8 +28,25 @@ def signin(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request)
+        user = authenticate(username=username, password=password)
         if user is not None:
-            login(username, password)
+            login(request, user)
             return redirect('homepage')
     return render(request, 'accounts/signin.html')
+
+
+"""
+def anyview():
+    Instead of using this way
+    if username is not empty:
+        register user
+    else:
+        return username is required
+
+    Using this way is much better
+    if user is empty:
+        return "Username is required"
+        
+    register logic
+
+"""
