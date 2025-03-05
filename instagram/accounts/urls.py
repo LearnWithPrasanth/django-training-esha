cc9371a6c0adc34e4778b498a58ca1faf1342139
Prasanth Kumar <training@prasanthkumarnagothi.com>
2025-03-05 11:31:34 +0530

    Custom Authentication and Class based views

--- instagram/accounts/urls.py
+++ instagram/accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup', views.signup, name='signup'),
+    path('signup', views.RegisterView.as_view(), name='signup'),
     path('signin', views.signin, name='signin'),
 ]
--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -2,51 +2,21 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+from .forms import RegistrationForm
 
 
-def signup(request):
-    # request_data = request.scheme
-    # print(request_data)
-    if request.method == 'POST':
-        first_name = request.POST.get('firstname')
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+class RegisterView(CreateView):
+    form_class = RegistrationForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('accounts:signin')
 
-        if User.objects.filter(username=username):
-            return render(request, 'accounts/signup.html')
-
-        User.objects.create_user(
-            first_name=first_name,
-            username=username,
-            password=password
-        )
-        return redirect('signin')
-    return render(request, 'accounts/signup.html')
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
 
 
 def signin(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('newsfeed:home')
-    return render(request, 'accounts/signin.html')
-
-
-"""
-def anyview():
-    Instead of using this way
-    if username is not empty:
-        register user
-    else:
-        return username is required
-
-    Using this way is much better
-    if user is empty:
-        return "Username is required"
-        
-    register logic
-
-"""
+    return HttpResponse('sign in')
