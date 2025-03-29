28e6f874177424b1208ad5dd128b69d61b1451a6
Prasanth Kumar <training@prasanthkumarnagothi.com>
2025-03-29 16:09:48 +0530

    DRF Authentication

--- DRF/accounts/views.py
+++ DRF/accounts/views.py
@@ -1,9 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
 
 @api_view(["POST"])
 def register(request):
-    data = request.data 
-    
+    data = request.data
+    serializer = UserSerializer(data=data)
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- DRF/drf/settings.py
+++ DRF/drf/settings.py
@@ -60,6 +60,15 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     )
+# }
+
 ROOT_URLCONF = 'drf.urls'
 
 TEMPLATES = [
